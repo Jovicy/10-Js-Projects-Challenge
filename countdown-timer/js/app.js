ec4497b82aa03414b0cb19368a03f60f08a1ae2f
Bamidele <bamsjovicy@gmail.com>
2023-01-17 00:02:46 +0100

    done with the first project

--- countdown-timer/js/app.js
+++ countdown-timer/js/app.js
@@ -1,17 +1,32 @@
+const daysElements = document.getElementById("days");
+const hoursElements = document.getElementById("hours");
+const minsElements = document.getElementById("mins");
+const secondsElements = document.getElementById("seconds");
+
 const newYears = '1 Jan 2024';
 
 function countDown() {
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
 
-    const seconds = (newYearsDate - currentDate) / 1000;
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
 
-    const days = Math.floor(seconds / 3600 / 24);
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds/ 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
 
-    console.log(days);
+    daysElements.innerHTML = days;
+    hoursElements.innerHTML = formatTime(hours);
+    minsElements.innerHTML = formatTime(minutes);
+    secondsElements.innerHTML = formatTime(seconds);
 }
 
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
 // Function call
 countDown();
+ 
 
 setInterval(countDown, 1000)
